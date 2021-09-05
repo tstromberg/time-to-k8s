6141a4cc6f9d7f86b863a2b42edf245180b902ec
Wei Luo <wluo416@gmail.com>
2021-09-04 17:32:41 -0700

    Output cpu time instead of cpu busy percentage

--- time-to-k8s.go
+++ time-to-k8s.go
@@ -42,7 +42,7 @@ type ExperimentResult struct {
 	ExitCode      int
 	Error         string
 	Timestamp     time.Time
-	CPUBusyPct    float64
+	CPUTime       time.Duration
 }
 
 // RunResult stores the result of an cmd.Run call
@@ -148,7 +148,7 @@ func runIteration(name string, setupCmd string, cleanupCmd string) (e Experiment
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		e.CPUBusyPct = cr.Run(ctx).Busy * 100
+		e.CPUTime = time.Duration(cr.Run(ctx).Busy * float64(e.Total.Nanoseconds()))
 	}()
 
 	wg.Add(1)
@@ -273,7 +273,7 @@ func main() {
 
 	c := csv.NewWriter(outputFile)
 
-	c.Write([]string{"name", "args", "platform", "iteration", "time", "version", "exitcode", "error", "command exec (seconds)", "apiserver answering (seconds)", "kubernetes svc (seconds)", "dns svc (seconds)", "app running (seconds)", "dns answering (seconds)", "cpu busy (percent)", "total duration (seconds)"})
+	c.Write([]string{"name", "args", "platform", "iteration", "time", "version", "exitcode", "error", "command exec (seconds)", "apiserver answering (seconds)", "kubernetes svc (seconds)", "dns svc (seconds)", "app running (seconds)", "dns answering (seconds)", "cpu time (seconds)", "total duration (seconds)"})
 	klog.Infof("Writing output to %s", outputFile.Name())
 	c.Flush()
 
@@ -320,7 +320,7 @@ func main() {
 				ds(e.DNSSvc),
 				ds(e.AppRunning),
 				ds(e.DNSAnswering),
-				fmt.Sprintf("%.2f", e.CPUBusyPct),
+				ds(e.CPUTime),
 				ds(e.Total),
 			}
 			c.Write(fields)
